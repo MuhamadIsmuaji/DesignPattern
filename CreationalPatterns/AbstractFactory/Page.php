127e15c5dc6e93d3975a81021ba78eadf5608bc1
Muhamad Ismuaji <muhamadismuaji@gmail.com>
2020-05-03 12:25:18 +0700

    (fix) move to new resource

--- CreationalPatterns/AbstractFactory/Page.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// namespace CreationalPatterns\AbstractFactory;
-
-class Page {
-    public function render(TemplateFactory $templateFactory) {
-        return $templateFactory->createPageTemplate();
-    }
-}
